791bb52ce8befc7113ecd8335e1f250c0b1ece45
Nikhil Chhipa <chhipanikhil9@gmail.com>
2021-05-07 19:48:51 +0530

    Create Do_Not_Be_Distracted!.cpp

---
+++ Codeforces/Do_Not_Be_Distracted!.cpp
@@ -0,0 +1,63 @@
+// Author : |\| | |< |-| | |_	( |-| |-| | |> /\ //
+
+#include<bits/stdc++.h>
+#define fastio 					ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define ll 						long long
+#define vi  					vector<int>
+#define vvi 					vector<vi>
+#define vl						vector<ll>
+#define vvl 					vector<vl>
+#define all(x)  				(x).begin(), (x).end()
+#define pb 						push_back
+#define ff 						first
+#define ss 						second//typedef second ss;
+#define unmap  					unordered_map<int,int>
+#define	unset					unordered_set<int>
+#define maxheap  				priority_queue<int>
+#define minheap  				priority_queue<int, vi, greater<int>>
+#define foton(i,n) 				for(int i=0;i<n;i++)
+#define	faton(i,a,n)			for(int i=a;i<=n;i++)
+#define endl 					"\n"
+#define isort(a) 				sort(all(a))
+#define dsort(a)				sort(all(a), greater<int>())
+const ll mod = (ll)1e9 + 7;
+const int inf = (int)1e6 + 7;
+using namespace std;
+
+int32_t main() {
+
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	freopen("error.txt","w",stderr);
+#endif
+
+	fastio;
+	int t; cin >> t;
+	while (t--) {
+		int n;cin>>n;
+		string s;cin>>s;
+		int a[26] = {0};
+		a[s[0]-'A']++;
+		int f = 0;
+		faton(i,1,n-1){
+			if(s[i]==s[i-1]){
+				continue;
+			}
+			else if(a[s[i]-'A'] > 0){
+				f = 1;
+				break;
+			}
+			else{
+				a[s[i]-'A']++;
+			}
+		}
+		if(f){
+			cout<<"NO"<<endl;
+		}
+		else{
+			cout<<"YES"<<endl;
+		}
+	}
+	return 0;
+}
